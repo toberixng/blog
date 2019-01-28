1d74a9a19d6e14b9b81271d3c12c91dd6a0e1798
Tobi Fatumo <tfatumo@emzorlan.com>
2019-01-28 18:29:53 +0100

    callback update

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 console.log('Before');
+getUser(1, (user) => {
+    // console.log('User', user);
+    
+    getRepositories(user.gitHubUsername, (repos) =>{
+        console.log('Repos', repos)
+    });
+});
+console.log('After');
 
-setTimeout(() => {
-    console.log('Reading a User from the database');
-}, 2000);
+function getUser(id, callback){
+    setTimeout(() => {
+        console.log('Reading a User from the database');
+        callback({ id: id, gitHubUsername: 'toberixng'});
+    }, 2000);    
+}
 
-console.log('After')
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log(`getting ${username} repo from the github database`)
+        callback(['repo1', 'repos2', 'repo3'])
+    }, 2000);
+}
