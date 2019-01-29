33eb07f95f7658d870b122d8fe9f7c98ae145e40
Tobi Fatumo <tfatumo@emzorlan.com>
2019-01-29 09:41:33 +0100

    promise all update

--- promise-api.js
+++ promise-api.js
@@ -1,3 +1,20 @@
 
-const p = Promise.resolve({ id: 1 });
-p.then(result => console.log(result));
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1.....');
+        reject(new Error('Because something failed.'));
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 1.....');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log('Error', err.message))
+
+// Promise.race([p1, p2])
