fe0047be1af2533f3a3a1dbdd871dbbb8bfb6589
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-08-19 18:40:32 +0100

    page update

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const http = require('http');
-const app = require('./app');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<p>hello, ninjas</p>');
+  res.write('<p>hello again, ninjas</p>');
+  res.end();
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
+
+
+
+// const http = require('http');
+// const app = require('./app');
 
 // const normalizePort = val => {
 //   const port = parseInt(val, 10);
