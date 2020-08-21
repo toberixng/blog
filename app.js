7080fa3834c81c45d78e8138a327ba2fc68f6574
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-08-21 18:31:28 +0100

    update

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 })
 
-app.get('/about-us', (req, res) => {
-    res.redirect('about')
-})
+// app.get('/about-us', (req, res) => {
+//     res.redirect('about')
+// })
 
-app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+// app.use((req, res) => {
+//     res.status(404).render('404')
+// })
